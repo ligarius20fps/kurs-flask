367693bf0adba09a73bc51c6bdaddceaf50ce61d
ligarius20fps <ligarius20fps@users.noreply.github.com>
2023-04-04 13:26:53 +0200

    funkcja abort i kawałek DELETE

--- smorest/app/app.py
+++ smorest/app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from db import stores, items
 from uuid import uuid4
+from flask_smorest import abort
 
 app = Flask(__name__)
 
@@ -20,7 +21,7 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": f"Store with ID {store_id} not found"}, 404
+        abort(404, message=f"Store with ID {store_id} not found")
 
 
 @app.get("/item/<string:item_id>")
@@ -28,12 +29,18 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": f"Item with ID {item_id} not found"}, 404
+        abort(404, message=f"Item with ID {item_id} not found")
 
 
 @app.post("/store")
 def add_store():
     req = request.get_json()
+    # name – string
+    if {"name"} != req.keys():
+        return abort(400, message="Bad Request. Only 'name' should be included in the JSON payload.")
+    for store in stores.values():
+        if store["name"] == req["name"]:
+            return abort(400, message="Bad Request. The store already exists.")
     store_id = uuid4().hex
     new_store = req | {"store_id": store_id}
     stores[store_id] = new_store
@@ -43,10 +50,29 @@ def add_store():
 @app.post("/item")
 def add_item_to_store():
     req = request.get_json()
+    # name – string
+    # price – float.2
+    # store_id – uuid
+    if {"name", "price", "store_id"} != req.keys():
+        return abort(400, message="Bad Request. "
+                                  "Only 'name', 'price' and 'store_id' should be included in the JSON payload.")
     store_id = req["store_id"]
+    for item in items.values():
+        if (item["name"] == req["name"] and
+                item["store_id"] == req["store_id"]):
+            return abort(400, message="Bad Request. The item already exists.")
     if store_id not in stores:
-        return {"message": f"Store with ID {store_id} not found"}, 404
+        abort(404, message=f"Store with ID {store_id} not found")
     item_id = uuid4().hex
     new_item = req | {"item_id": item_id}
     items[item_id] = new_item
     return new_item
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message=f"Item not found")
