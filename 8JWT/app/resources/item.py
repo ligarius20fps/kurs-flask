d57d6874a1b3162b8d8b088de45d50b55a978a9c
ligarius20fps <ligarius20fps@users.noreply.github.com>
2023-04-09 17:24:29 +0200

    udokumentowano towary

--- 8JWT/app/resources/item.py
+++ 8JWT/app/resources/item.py
@@ -12,30 +12,37 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 @blp.route("/item")
 class Item(MethodView):
     @blp.arguments(ItemSchema)
-    @blp.response(201, ItemSchema)
+    @blp.response(201, ItemSchema, description="Adds an item")
+    @blp.alt_response(500, description="Returned if there is an error in the database")
     def post(self, req):
         new_item = ItemModel(**req)
         try:
             db.session.add(new_item)
             db.session.commit()
-        except SQLAlchemyError:
-            abort(500, message="An error occured")
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
         return new_item, 201
 
-    @blp.response(200, ItemSchema(many=True))
+    @blp.response(200, ItemSchema(many=True), description="Returns all items in the database")
     def get(self):  # http://127.0.0.1:5000/item
         return ItemModel.query.all()
 
 
 @blp.route("/item/<string:item_id>")
 class ItemID(MethodView):
-    @blp.response(200, ItemSchema)
+    @blp.response(200, ItemSchema, description="Returns an item with provided ID")
+    @blp.alt_response(404, description="Returned if no item found with such ID",
+                      example={
+                        "code": 404,
+                        "status": "Not Found"
+                    })
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
 
     @blp.arguments(ItemUpdateSchema)
-    @blp.response(200, ItemSchema)
+    @blp.response(200, ItemSchema, description="Creates or updates an item in the database")
+    @blp.alt_response(500, description="Returned if there is an error in the database")
     def put(self, req, item_id):
         item = ItemModel.query.get(item_id)
         if item:
@@ -46,10 +53,16 @@ class ItemID(MethodView):
         try:
             db.session.add(item)
             db.session.commit()
-        except SQLAlchemyError:
-            abort(500, message="An error occured")
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
         return item
 
+    @blp.response(200, description="Deletes an item with provided ID", example={"message": "Item successfully deleted"})
+    @blp.alt_response(404, description="Returned if no item found with such ID",
+                      example={
+                        "code": 404,
+                        "status": "Not Found"
+                    })
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
