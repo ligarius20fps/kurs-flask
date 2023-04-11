4f01b8164e3c8b15b488bd9228e82bd5c008e2d2
ligarius20fps <ligarius20fps@users.noreply.github.com>
2023-04-11 17:20:30 +0200

    Model i Schemat użytkownika

--- 8JWT/app/schemas.py
+++ 8JWT/app/schemas.py
@@ -44,3 +44,8 @@ class ItemAndTagSchema(Schema):
     message = fields.Str()
     item = fields.Nested(ItemSchema)
     tag = fields.Nested(TagSchema)
+
+class UserSchema(Schema):
+    user_id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
